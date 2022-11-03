3bd8a6d4958dc6135a25eca889153d70e2e5f60f
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 13:56:00 +0100

    Updated Controller

--- dynamicController/src/main/java/com/bosonic/dynamicController/controller/Controller.java
+++ dynamicController/src/main/java/com/bosonic/dynamicController/controller/Controller.java
@@ -3,7 +3,11 @@ package com.bosonic.dynamicController.controller;
 import com.bosonic.dynamicController.application.ServiceControllerObject;
 import com.bosonic.dynamicController.domain.ControllerObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +16,12 @@ public class Controller {
     @Autowired
     private ServiceControllerObject sc;
 
-    @GetMapping("")
-    public String entryOther() {
-        return "You has passed to entryOther zone";
+    @GetMapping(value={"*","{path}","{path}/{path2}"})
+    public ControllerObject entryOther(@PathVariable(required = false) String path, @PathVariable (required = false) String path2,
+                                                       @RequestParam (required = false) String nombre,
+                                                       @RequestParam (required = false) String ciudad) {
+
+        return sc.returnPath(path, path2, nombre, ciudad);
     }
 
     @GetMapping(value={"/","one"})
