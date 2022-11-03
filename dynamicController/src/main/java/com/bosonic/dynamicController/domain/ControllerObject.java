c56c8b50113f723b189f9566506e42770eb2e73e
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 14:06:28 +0100

    Updated ControllerObject

--- dynamicController/src/main/java/com/bosonic/dynamicController/domain/ControllerObject.java
+++ dynamicController/src/main/java/com/bosonic/dynamicController/domain/ControllerObject.java
@@ -1,16 +1,38 @@
 package com.bosonic.dynamicController.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@AllArgsConstructor
+
 @Data
 public class ControllerObject {
 
-    private static List<String> paths;
-    private static List<Map> headers;
+    private List<String> paths;
+    private Map <String, String> headers;
+
+    public ControllerObject(){
+        paths= new ArrayList<>();
+        headers = new HashMap<String, String>();
+    }
+
+    public  List<String> getPaths() {
+        return paths;
+    }
+
+    public Map<String, String> getHeaders(){
+        return headers;
+    }
+
+    public void addPath(String path){
+        paths.add(path);
+
+    }
+    public void addHeader(String key1, String key2){
+        headers.put(key1, key2);
+    }
+
 
 }
