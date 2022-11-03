5c5cf68d00b592bf787ce161e3e1be09dd252092
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 14:14:19 +0100

    Updated ControllerObject to add headers propertie

--- dynamicController/src/main/java/com/bosonic/dynamicController/domain/ControllerObject.java
+++ dynamicController/src/main/java/com/bosonic/dynamicController/domain/ControllerObject.java
@@ -1,6 +1,7 @@
 package com.bosonic.dynamicController.domain;
 
 import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,26 +12,38 @@ import java.util.Map;
 public class ControllerObject {
 
     private List<String> paths;
-    private Map <String, String> headers;
+    private Map<String, String> querys;
 
-    public ControllerObject(){
-        paths= new ArrayList<>();
+    private Map<String, String> headers;
+
+    public ControllerObject() {
+        paths = new ArrayList<>();
+        querys = new HashMap<String, String>();
         headers = new HashMap<String, String>();
     }
 
-    public  List<String> getPaths() {
+    public List<String> getPaths() {
         return paths;
     }
 
-    public Map<String, String> getHeaders(){
+    public Map<String, String> getQuerys() {
+        return querys;
+    }
+
+    public Map<String, String> getHeaders() {
         return headers;
     }
 
-    public void addPath(String path){
+    public void addPath(String path) {
         paths.add(path);
 
     }
-    public void addHeader(String key1, String key2){
+
+    public void addQuery(String key1, String key2) {
+        querys.put(key1, key2);
+    }
+
+    public void addHeader(String key1, String key2) {
         headers.put(key1, key2);
     }
 
