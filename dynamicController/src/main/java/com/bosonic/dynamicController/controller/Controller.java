c65b53f27569a178fdb13331fd69f95b050d5acb
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 14:47:59 +0100

    Updated ControllerObject to Map

--- dynamicController/src/main/java/com/bosonic/dynamicController/controller/Controller.java
+++ dynamicController/src/main/java/com/bosonic/dynamicController/controller/Controller.java
@@ -3,12 +3,9 @@ package com.bosonic.dynamicController.controller;
 import com.bosonic.dynamicController.application.ServiceControllerObject;
 import com.bosonic.dynamicController.domain.ControllerObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class Controller {
@@ -16,16 +13,17 @@ public class Controller {
     @Autowired
     private ServiceControllerObject sc;
 
-    @GetMapping(value={"*","{path}","{path}/{path2}"})
-    public ControllerObject entryOther(@PathVariable(required = false) String path, @PathVariable (required = false) String path2,
-                                                       @RequestParam (required = false) String nombre,
-                                                       @RequestParam (required = false) String ciudad) {
+    @GetMapping(value = {"*", "{path}", "{path}/{path2}"})
+    public ControllerObject entryOther(@PathVariable(required = false) String path,
+                                       @PathVariable(required = false) String path2,
+                                       @RequestParam(required = false) Map<String, String> query,
+                                       @RequestHeader (required = false) Map<String, String> header) {
 
-        return sc.returnPath(path, path2, nombre, ciudad);
+        return sc.returnPath(path, path2, query, header);
     }
 
-    @GetMapping(value={"/","one"})
-    public String entryOne(){
+    @GetMapping(value = {"/", "one"})
+    public String entryOne() {
 
         return "You has passed to entryOne zone";
 
