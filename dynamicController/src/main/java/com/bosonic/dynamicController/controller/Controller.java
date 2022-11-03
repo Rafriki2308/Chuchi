13469cbe1d6f05e20440f0810dde8a6965cb3bee
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 17:03:22 +0100

    Added filter to body

--- dynamicController/src/main/java/com/bosonic/dynamicController/controller/Controller.java
+++ dynamicController/src/main/java/com/bosonic/dynamicController/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
@@ -21,10 +22,16 @@ public class Controller {
                              @PathVariable(required = false) String path2,
                              @RequestParam(required = false) Map<String, String> query,
                              @RequestHeader(required = false) Map<String, String> header,
-                             HttpServletRequest request) {
+                             @RequestBody (required = false) String body,
+                             HttpServletRequest request,
+                             ServletResponse response) {
 
         if (request.getHeader("REDIRIGE") != null && request.getHeader("REDIRIGE").equals("SALTA")) {
             return new ModelAndView("redirect:/salta");
+        }
+
+        if(request.getHeader("ADD") != null && body != null){
+            return body + " modificado";
         }
 
         return sc.returnPath(path, path2, query, header);
