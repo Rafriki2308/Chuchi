77ccb40e116695b32b541bb2316685bd1f0627d4
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 12:00:08 +0100

    Initial commit

---
+++ dynamicController/src/main/java/com/bosonic/dynamicController/DynamicControllerApplication.java
@@ -0,0 +1,13 @@
+package com.bosonic.dynamicController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicControllerApplication.class, args);
+	}
+
+}
