07d88fdd941217a630f7613bd8f51bbcd8a117e5
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-03 14:06:44 +0100

    Updated ServiceControllerObject

--- dynamicController/src/main/java/com/bosonic/dynamicController/application/ServiceControllerObject.java
+++ dynamicController/src/main/java/com/bosonic/dynamicController/application/ServiceControllerObject.java
@@ -1,7 +1,32 @@
 package com.bosonic.dynamicController.application;
 
+import com.bosonic.dynamicController.domain.ControllerObject;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ServiceControllerObject {
+
+    public static ControllerObject controllerObject = new ControllerObject();
+    public ControllerObject returnPath(String path, String path2, String nombre, String ciudad){
+
+        if(path!=null){
+            controllerObject.addPath(path);
+        }
+        if(path2!=null){
+            controllerObject.addPath(path2);
+        }
+
+        if(nombre!=null){
+            controllerObject.addHeader("nombre", nombre);
+        }
+
+        if(ciudad!=null){
+            controllerObject.addHeader("ciudad", ciudad);
+        }
+
+        return controllerObject;
+    }
 }
